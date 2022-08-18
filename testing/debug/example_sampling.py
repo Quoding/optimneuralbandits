6e147e76f6202ad7dcad5ab19252d5e48ca00a77
Quoding <46576765+Quoding@users.noreply.github.com>
2022-08-18 15:42:57 -0400

    Update example_sampling.py

--- testing/debug/example_sampling.py
+++ testing/debug/example_sampling.py
@@ -1,40 +1,51 @@
-import sqlite3 as sql
 import threading
 import time
+from queue import Queue
+import sqlite3 as sql
+import pandas as pd
 
 con = sql.connect("chinook.db", check_same_thread=False)
-
 statement = "SELECT * FROM EMPLOYEES;"
 
-current_data = []
-next_data = []
-time_to_sample = True
+algo_over = False
 
 
-def fetch_data(con):
-    # Fetch the data from the database
-    global time_to_sample
-    global next_data
+def fetch_data(con: sql.Connection, output: Queue):
+    global algo_over
     cur = con.cursor()
-    next_data = cur.execute(statement).fetchall()
-    time.sleep(10)
-    time_to_sample = True
-    print("Fetched data")
+    cnt = 0
+    while not algo_over:
+        cnt += 1
+        df = pd.read_sql(statement, con=con)[:cnt]
+        time.sleep(10)
+        output.put(df)
+        print("Fetched data")
 
 
-fetch_data(con)
-current_data = next_data
+def main():
+    global algo_over
 
-for i in range(100):
+    q = Queue()
+    db_thread = threading.Thread(target=fetch_data, args=[con, q])
+    db_thread.start()
+    current_data = None
 
-    # Sample if a thread isn't already sampling the database
-    if time_to_sample:
-        print("Fetching data")
-        db_thread = threading.Thread(target=fetch_data, args=[con])
-        time_to_sample = False
-        db_thread.start()
+    # Make sure we have a first sample before starting
+    # while current_data is None:
+    #     if q:
+    #         current_data = q.get()
 
-    # Update current_data to the data we just fetched with the thread
-    current_data = next_data
-    time.sleep(1)
-    print(f"Executed {i}th loop")
+    for i in range(100):
+        time.sleep(1)
+        if not q.empty():
+            current_data = q.get()
+        print(f"Executed {i}")
+
+    algo_over = True
+
+
+try:
+    main()
+except:
+    # If anything happens and main crashes, make sure the thread exits
+    algo_over = True
