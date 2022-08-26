e4238256464d76f0c069516c4b42cfaaf01b3a82
Quoding <quoding@protonmail.ch>
2022-08-26 13:36:21 -0400

    Remove debug directory from repo

--- testing/debug/reduce.sh
+++
@@ -1,8 +0,0 @@
-for d in $(find runs -maxdepth 2 -mindepth 2 -type d)
-do
-  #Do something, the directory is accessible with $d:
-  tb-reducer ${d}/*  -o ${d}_reduced -r mean,std,min,max --lax-steps
-done
-
-
-
